0e8ccb3f8cf703549a69d8f857df7635443f8376
InternetKungen <kandersson@gmail.com>
2023-10-16 22:43:59 +0200

    ordnat böckernas artiklar

--- src/js/script.js
+++ src/js/script.js
@@ -1,8 +1,11 @@
 function toggleMenu() {
     const section_menu = document.querySelector('.menu-container');
     const menuButton = document.querySelector('.menu-button');
+
     section_menu.classList.toggle('hidden');
-    menuButton.classList.toggle('open'); // Lägg till eller ta bort CSS-klassen 'open'
+    menuButton.classList.toggle('open'); // Lägg till eller ta bort CSS-klassen 'open' 
+
+
 }
 
 function toggleSubMenu(link) {
